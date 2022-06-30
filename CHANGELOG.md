56dd2541803f58b9c3d0aa92c9b6bf60d32ebccf
Erkin Ovlyagulyyev <mail@erkin.me>
2022-06-30 15:07:58 +0500

    feat: update android version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+## 2.0.1
+
+* Migration to MavenCentral
+* Small improvements
+
+## 2.0.0
+
+* Add null-safety
+
 ## 1.0.2
 
 * Update Android compileSdkVersion to 28
--- lib/store_redirect.dart
+++ lib/store_redirect.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 class StoreRedirect {
   static const MethodChannel _channel = const MethodChannel('store_redirect');
 
-  /// Note: It will not work with the iOS Simulator
   static Future<void> redirect({String? androidAppId, String? iOSAppId}) async {
     await _channel.invokeMethod(
         'redirect', {'android_id': androidAppId, 'ios_id': iOSAppId});
